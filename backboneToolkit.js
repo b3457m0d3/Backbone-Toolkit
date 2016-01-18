97776f91bc971c37df83e624e114ffcfd6bcc482
Jessie Taylor <b3457m0d3@interr0bang.net>
2016-01-17 22:02:20 -0500

    boilerplate
    start here

---
+++ backboneToolkit.js
@@ -0,0 +1,8 @@
+define(['jquery','_','backbone','blueprint'], function($,_,Backbone) { 
+  var Toolkit = Backbone.Collection.extend(_.extend({}, {
+
+    // (Methods and attributes here)
+
+  }));
+  Backbone.Toolkit;
+});
